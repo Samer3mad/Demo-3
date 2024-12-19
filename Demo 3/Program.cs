8d7a1ff69a52633a1be2784a99e5a70cab4aadd9
Samer Zaki <Samer.Zaki22@svnu.edu.eg>
2024-12-19 11:46:56 +0200

    Value Type Casting

--- Demo 3/Program.cs
+++ Demo 3/Program.cs
@@ -7,7 +7,7 @@
             #region Value Type Casting
             #region Implicict Casting - Safe Casting
             //int x = 4; //4 Bytes
-                       //long y = /*(long)*/x;
+            //long y = /*(long)*/x;
             #endregion
 
             #region Explict Casting
@@ -22,6 +22,32 @@
             //Console.WriteLine(y);
             //int x = 8;
             //double y = (double)x;
+            #endregion
+
+            #region Parse
+            //int num = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Enter Data User");
+            //Console.Write("Enter Name: ");
+            //string Name=Console.ReadLine();
+            //Console.Write("Enter Age: ");
+            //int age = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Enter Salary: ");
+            //double Salary = double.Parse(Console.ReadLine());
+            //Console.Clear();
+            //Console.WriteLine("Name: " + Name + "Age: " + age + "Salary: " + Salary);
+            #endregion
+
+            #region Convert
+            //int num = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Enter Data User");
+            //Console.Write("Enter Name: ");
+            //string Name = Console.ReadLine();
+            //Console.Write("Enter Age: ");
+            //int age = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Enter Salary: ");
+            //double Salary = Convert.ToDouble(Console.ReadLine());
+            //Console.Clear();
+            //Console.WriteLine("Name: " + Name + "Age: " + age + "Salary: " + Salary);
             #endregion
             #endregion
         }
