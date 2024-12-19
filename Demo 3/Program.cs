a1acaf7a07544fac534f49541a4255af8faeafcd
Samer Zaki <Samer.Zaki22@svnu.edu.eg>
2024-12-19 11:33:18 +0200

    Video 1 - Value Type Casting

--- Demo 3/Program.cs
+++ Demo 3/Program.cs
@@ -4,7 +4,26 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Value Type Casting
+            #region Implicict Casting - Safe Casting
+            //int x = 4; //4 Bytes
+                       //long y = /*(long)*/x;
+            #endregion
+
+            #region Explict Casting
+            //long x = 101010101010; //8 Bytes
+
+            //int y;
+            //checked
+            //{
+            //    y = (int)x;
+
+            //}
+            //Console.WriteLine(y);
+            //int x = 8;
+            //double y = (double)x;
+            #endregion
+            #endregion
         }
     }
 }
